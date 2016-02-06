d933c47ccd2d28a4976e83188b28b61decfed214
Gon <paarp2015@mail.ru>
2016-02-06 11:11:18 +0200

    initial commit

---
+++ pick_a_card.rb
@@ -0,0 +1,6 @@
+values = ['2','3','4','5','6','7','8','9','10','J','Q','K','A']#Значение
+suits = ['Diamonds', 'Hearts', 'Clubs', 'Spades']# Масть
+# Выводи комбинацию значения и массива
+puts "#{values.sample} of #{suits.sample}"
+
+
